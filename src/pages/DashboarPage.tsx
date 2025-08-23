2e635787ced4fe4574f32d63fb39a1262f84d988
bertolucciDev <devluisbertolucci@gmail.com>
2025-08-22 23:39:33 -0300

    Feat: update Dashboard and Login page, add logo, update dependencies

--- src/pages/DashboarPage.tsx
+++ src/pages/DashboarPage.tsx
@@ -8,8 +8,7 @@ export default function Dashboard() {
         items={[{ icon: <Home size={15} />, label: "Início" }]}
         footer={
           <button className="flex items-center gap-2 text-sm text-gray-600 hover:text-black">
-            <UserCog size={16} />,
-            Configurações do Usuario
+            <UserCog size={16} />, Configurações do Usuario
           </button>
         }
       ></Sidebar>
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,66 +2,111 @@ import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
-  CardFooter,
   CardHeader,
-  CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { User, Lock, Eye, EyeOff } from "lucide-react";
+import Swal from "sweetalert2"
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
 
   const handleLogin = (e: React.FormEvent) => {
     e.preventDefault();
 
     if (password.trim() === "" || email.trim() === "") {
-      alert("Preencha todos os campos");
+      Swal.fire({
+        icon: 'error',
+        title: 'Algo deu errado!',
+        text: 'Preencha todos os campos!',
+        confirmButtonColor: '#14B8A6'
+      })
       return;
     }
 
-    navigate("/dashboard");
+    Swal.fire({
+      icon: 'success',
+      title: 'Login realizado com sucesso!',
+      text: 'Redirecionando!',
+      showConfirmButton: false,
+      timer: 2000,
+    }).then(() => {
+      navigate("/dashboard");
+    })
   };
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <Card className="w-md">
-        <CardHeader>
-          <CardTitle>Login</CardTitle>
-        </CardHeader>
-        <form onSubmit={handleLogin}>
-          <CardContent>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
+    <div className="flex h-screen">
+      <div className="w-full md:w-1/2 flex items-center justify-center bg-gray-100">
+        <Card className="W-[350px] border-none shadow-none bg-gray-100">
+          <CardHeader className="flex flex-col items-center">
+            {/* Logo */}
+            <div className="flex flex-col items-center">
+              <div className="flex items-center justify-center">
+                <img src="./public/OrganizationTechLogo.png" />
+              </div>
+            </div>
+          </CardHeader>
+
+          <form onSubmit={handleLogin}>
+            <CardContent className="space-y-4">
+              {/* Input com ícone User */}
+              <div className="relative flex items-center">
+                <span className="absolute left-3 text-gray-500">
+                  <User size={18} />
+                </span>
                 <Input
                   id="email"
                   type="email"
                   onChange={(e) => setEmail(e.target.value)}
-                  placeholder="you@exemplo.com"
+                  placeholder="Insira Seu Email"
+                  className="pl-10"
                 />
               </div>
-              <div className="grid gap-2">
-                <Label htmlFor="password">Password</Label>
+
+              {/* Input com ícone Lock e botão de olho */}
+              <div className="relative flex items-center">
+                <span className="absolute left-3 text-gray-500">
+                  <Lock size={18} />
+                </span>
                 <Input
                   id="password"
-                  type="password"
+                  type={showPassword ? "text" : "password"}
                   onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Insira Sua Senha"
+                  className="pl-10 pr-10"
                 />
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute right-3 text-gray-500"
+                >
+                  {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+                </button>
               </div>
-            </div>
-          </CardContent>
-          <CardFooter className="flex-col py-2 p-6 gap-2">
-            <Button type="submit" variant={"outline"} className="w-full">
-              Entrar
-            </Button>
-          </CardFooter>
-        </form>
-      </Card>
+
+              {/* Botão de login */}
+              <Button
+                type="submit"
+                className="w-full bg-teal-500  hover:bg-teal-600 text-white"
+              >
+                Entrar
+              </Button>
+            </CardContent>
+          </form>
+        </Card>
+      </div>
+
+      <div className="w-1/1 bg-gradient-to-b hidden md:block"
+        style={{
+          background: "linear-gradient(to bottom, rgba(18,48,115,0.772), #2563eb)"
+        }}
+      />
     </div>
   );
 }
